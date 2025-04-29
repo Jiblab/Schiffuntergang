401eca3ec53644002326e91fefa8f3764114f5b3
Sini_20 <sinem032003@gmail.com>
2025-04-29 09:37:29 +0200

    startscreen

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,14 +2,17 @@ module org.example.schiffuntergang {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires javafx.media;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
-    //requires eu.hansolo.tilesfx;
+
     requires com.almasb.fxgl.all;
 
     opens org.example.schiffuntergang to javafx.fxml;
+
     exports org.example.schiffuntergang;
 }
+
